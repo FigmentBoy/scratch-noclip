7f7a1df9d63c23530090d6ea00c82e4067021b25
FigmentBoy <17240592+FigmentBoy@users.noreply.github.com>
2020-12-27 04:37:52 -0600

    Changed to be an exposed memory thing instead of hardcoded

--- app.py
+++ app.py
@@ -4,17 +4,14 @@ from flask import Flask
 app = Flask(__name__)
 
 try:
-    gd = gd.memory.get_memory()
+    mem = gd.memory.get_memory()
 except:
     print("Please open GD!")
     exit(1)
 
-@app.route('/on')
-def on():
-    gd.enable_noclip()
-    return "Turned on"
-
-@app.route('/off')
-def off():
-    gd.disable_noclip()
-    return "Turned off"
+@app.route('/write/<pointer>/<byte>')
+def on(pointer, byte):
+    a = bytes([int(a, 16) for a in pointer.split(",")])
+    b = int(byte, 16)
+    mem.write_bytes(gd.memory.Buffer(a), b)
+    return "ok"
